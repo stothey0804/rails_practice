077af626eb45872d6b25ef2e726897013a750017
Seyoung Kim <seyoung.kim@reebonz.co.kr>
2020-12-14 15:43:17 +0900

    rails guide example

--- blog/app/models/post.rb
+++ blog/app/models/post.rb
@@ -1,8 +1,12 @@
 class Post < ActiveRecord::Base
-  attr_accessible :content, :name, :title
+  attr_accessible :content, :name, :title, :tags_attributes
   # 모든 Post는 name과 title을 가지고있어야 함.
   validates :name,  :presence => true
   validates :title, :presence => true,
                     :length => { :minimum => 5 } # 제목은 5글자 이상.
-  has_many :comments
+  has_many :comments, :dependent => :destroy
+  has_many :tags
+
+  accepts_nested_attributes_for :tags, :allow_destroy => :true,
+                                :reject_if => proc { |attrs| attrs.all? { |k, v| v.blank? } }
 end
